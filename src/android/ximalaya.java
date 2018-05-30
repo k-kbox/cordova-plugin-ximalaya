100e72e875bdd3baa005597520957a6fafc29ec2
jun.huang <huang@guru-tech.cn>
2018-05-30 17:14:32 +0800

    0.0.1

--- src/android/ximalaya.java
+++ src/android/ximalaya.java
@@ -1,15 +1,20 @@
 package com.cordova.plugins.ximalaya;
 
+import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
+import com.ximalaya.ting.android.opensdk.datatrasfer.IDataCallBack;
+import com.ximalaya.ting.android.opensdk.model.category.CategoryList;
+
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
-
-import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
 
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * This class echoes a string called from JavaScript.
@@ -31,7 +36,8 @@ public class ximalaya extends CordovaPlugin {
             return true;
         }
         else if (action.equals("getCategories")) {
-
+            Map<String, String> map = new HashMap<String, String>();
+            this.getCategories(map, callbackContext);
         }
         return false;
     }
@@ -40,6 +46,38 @@ public class ximalaya extends CordovaPlugin {
         CommonRequest.getInstanse().setAppkey(appKey);
         CommonRequest.getInstanse().setPackid(packId);
         CommonRequest.getInstanse().init(cordova.getActivity().getApplicationContext(), appSecret);
+    }
+
+    private void getCategories(Map<String, String> map, CallbackContext callbackContext) {
+        // Map<String, String> map = new HashMap<String, String>();
+        CommonRequest.getCategories(map, new IDataCallBack<CategoryList>() {
+            @Override
+            public void onSuccess(CategoryList object) {
+                JSONObject json = new JSONObject();
+                try {
+                    json.put("code", 0);
+                    json.put("message", "success");
+                    json.put("categories", new JSONArray(object.getCategories()));
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+                callbackContext.success(json.toString());
+            }
+​
+            @Override
+            public void onError(int code, String message) {
+                JSONObject json = new JSONObject();
+                try {
+                    json.put("code", code);
+                    json.put("message", message);
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+                callbackContext.error(json.toString());
+            }
+        });
     }
 
     private void coolMethod(String message, CallbackContext callbackContext) {
