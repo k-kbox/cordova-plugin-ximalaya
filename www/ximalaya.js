af22fa5b97d624027caf7977ea76d8ab70be4d3e
jun.huang <huang@guru-tech.cn>
2018-05-30 17:21:56 +0800

    0.0.1

--- www/ximalaya.js
+++ www/ximalaya.js
@@ -3,3 +3,11 @@ var exec = require('cordova/exec');
 exports.coolMethod = function (arg0, success, error) {
     exec(success, error, 'ximalaya', 'coolMethod', [arg0]);
 };
+
+exports.init = function (arg0, arg1, arg2, success, error) {
+    exec(success, error, 'ximalaya', 'init', [arg0, arg1, arg2]);
+}
+
+exports.getCategories = function (arg0, success, error) {
+  exec(success, error, 'ximalaya', 'getCategories', [arg0]);
+}
