88ac9f844ac213758e46241ac3cb6fd23a3cc1b6
jun.huang <huang@guru-tech.cn>
2018-06-13 17:50:32 +0800

    0.0.1

--- src/android/Ximalaya.java
+++ src/android/Ximalaya.java
@@ -1,13 +1,14 @@
 package com.cordova.plugins.ximalaya;
 
 import com.google.gson.Gson;
-import com.ximalaya.ting.android.opensdk.constants.DTransferConstants;
+//import com.ximalaya.ting.android.opensdk.constants.DTransferConstants;
 import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
 import com.ximalaya.ting.android.opensdk.datatrasfer.IDataCallBack;
-import com.ximalaya.ting.android.opensdk.model.album.AlbumList;
-import com.ximalaya.ting.android.opensdk.model.album.CategoryRecommendAlbumsList;
-import com.ximalaya.ting.android.opensdk.model.category.CategoryList;
-import com.ximalaya.ting.android.opensdk.model.tag.TagList;
+//import com.ximalaya.ting.android.opensdk.model.album.AlbumList;
+//import com.ximalaya.ting.android.opensdk.model.album.CategoryRecommendAlbumsList;
+//import com.ximalaya.ting.android.opensdk.model.category.CategoryList;
+//import com.ximalaya.ting.android.opensdk.model.tag.TagList;
+//import com.ximalaya.ting.android.opensdk.model.track.TrackList;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
@@ -16,7 +17,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.lang.reflect.Method;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 /**
@@ -33,46 +36,137 @@ public class Ximalaya extends CordovaPlugin {
         }*/
 
         if (action.equals("init")) {
-            this.init(args);
+//            this.init(args);
+            String appKey = args.getString(0);
+            String packId = args.getString(1);
+            String appSecret = args.getString(2);
+            CommonRequest.getInstanse().setAppkey(appKey);
+            CommonRequest.getInstanse().setPackid(packId);
+            CommonRequest.getInstanse().init(cordova.getActivity().getApplicationContext(), appSecret);
+            return true;
+        }
+        else {
+            try {
+                Method method = CommonRequest.class.getDeclaredMethod(action, Map.class, IDataCallBack.class);
+                Map<String, String> map = new HashMap<String, String>();
+
+                if (args.length() > 0) {
+                    JSONObject j = new JSONObject(args.getString(0));
+                    Iterator<String> iter = j.keys();
+                    while (iter.hasNext()) {
+                        String k = iter.next();
+                        map.put(k, j.optString(k, ""));
+                    }
+                }
+//                map.put(DTransferConstants.ALBUM_ID, args.getString(0));
+//                map.put(DTransferConstants.SORT, "asc");
+//                map.put(DTransferConstants.PAGE, args.getString(1));
+                method.invoke(null, map, createDataCallback(callbackContext));
+            }
+            catch (Exception e) {
+                JSONObject json = new JSONObject();
+                try {
+                    json.put("code", -1);
+                    json.put("message", e.getMessage());
+                }
+                catch (Exception e1) {
+                    e.printStackTrace();
+                }
+                callbackContext.error(json.toString());
+            }
             return true;
         }
 
-        if (action.equals("getCategories")) {
-            this.getCategories(args, callbackContext);
-            return true;
-        }
+//        if (action.equals("getCategories")) {
+//            this.getCategories(args, callbackContext);
+//            return true;
+//        }
+//
+//        if (action.equals("getTags")) {
+//            this.getTags(args, callbackContext);
+//            return true;
+//        }
+//
+//        if (action.equals("getAlbumList")) {
+//            this.getAlbumList(args, callbackContext);
+//            return true;
+//        }
+//
+//        if (action.equals("getTracks")) {
+//            this.getTracks(args, callbackContext);
+//            return true;
+//        }
+//
+//        if (action.equals("getCategoryRecommendAlbums")) {
+//            this.getCategoryRecommendAlbums(args, callbackContext);
+//            return true;
+//        }
 
-        if (action.equals("getTags")) {
-            this.getTags(args, callbackContext);
-            return true;
-        }
 
-        if (action.equals("getAlbumList")) {
-            this.getAlbumList(args, callbackContext);
-            return true;
-        }
 
-        if (action.equals("getCategoryRecommendAlbums")) {
-            this.getCategoryRecommendAlbums(args, callbackContext);
-            return true;
-        }
-
-        return false;
+//        return false;
     }
 
-    private void getCategoryRecommendAlbums(JSONArray args, CallbackContext callbackContext) throws JSONException {
-        Map<String, String> map = new HashMap<String, String>();
-        map.put(DTransferConstants.CATEGORY_ID, args.getString(0));
-        map.put("display_count" ,"10");
-        CommonRequest.getCategoryRecommendAlbums(map,
-            new IDataCallBack<CategoryRecommendAlbumsList>() {
+//    private void getTracks(JSONArray args, CallbackContext callbackContext) throws JSONException {
+//        Map<String, String> map = new HashMap<String, String>();
+//        map.put(DTransferConstants.ALBUM_ID, args.getString(0));
+//        map.put(DTransferConstants.SORT, "asc");
+//        map.put(DTransferConstants.PAGE, args.getString(1));
+//        CommonRequest.getTracks(map,
+//                (IDataCallBack<TrackList>) createDataCallback(callbackContext));
+//    }
+//
+//    private void getCategoryRecommendAlbums(JSONArray args, CallbackContext callbackContext) throws JSONException {
+//        Map<String, String> map = new HashMap<String, String>();
+//        map.put(DTransferConstants.CATEGORY_ID, args.getString(0));
+//        map.put("display_count" ,"10");
+//        CommonRequest.getCategoryRecommendAlbums(map,
+//                (IDataCallBack<CategoryRecommendAlbumsList>) createDataCallback(callbackContext));
+//    }
+//
+//    private void getAlbumList(JSONArray args, CallbackContext callbackContext) throws JSONException {
+//        Map<String, String> map = new HashMap<String, String>();
+//        map.put(DTransferConstants.CATEGORY_ID, args.getString(0));
+//        map.put(DTransferConstants.TAG_NAME, args.getString(1));
+//        map.put(DTransferConstants.CALC_DIMENSION ,"1");
+//        map.put(DTransferConstants.PAGE, args.getString(2));
+//
+//        CommonRequest.getAlbumList(map,
+//                (IDataCallBack<AlbumList>) createDataCallback(callbackContext));
+//    }
+//
+//    private void getTags(JSONArray args, CallbackContext callbackContext) throws JSONException {
+//        Map<String, String> map = new HashMap<String, String>();
+//        map.put(DTransferConstants.CATEGORY_ID, args.getString(0));
+//        map.put(DTransferConstants.TYPE, "0");
+//        CommonRequest.getTags(map,
+//                (IDataCallBack<TagList>) createDataCallback(callbackContext));
+//    }
+//
+//    private void getCategories(JSONArray args, CallbackContext callbackContext) throws JSONException {
+//        Map<String, String> map = new HashMap<String, String>();
+//        CommonRequest.getCategories(map,
+//                (IDataCallBack<CategoryList>) createDataCallback(callbackContext));
+//    }
+//
+//    private void init(JSONArray args) throws JSONException {
+//        String appKey = args.getString(0);
+//        String packId = args.getString(1);
+//        String appSecret = args.getString(2);
+//        CommonRequest.getInstanse().setAppkey(appKey);
+//        CommonRequest.getInstanse().setPackid(packId);
+//        CommonRequest.getInstanse().init(cordova.getActivity().getApplicationContext(), appSecret);
+//    }
+
+    private IDataCallBack<?> createDataCallback(CallbackContext callbackContext) {
+        return new IDataCallBack<Object>() {
             @Override
-            public void onSuccess(CategoryRecommendAlbumsList object) {
+            public void onSuccess(Object o) {
                 JSONObject json = new JSONObject();
                 try {
                     json.put("code", 0);
                     json.put("message", "success");
-                    json.put("data", new JSONObject(new Gson().toJson(object)));
+                    json.put("data", new JSONObject(new Gson().toJson(o)));
                 }
                 catch (Exception e) {
                     e.printStackTrace();
@@ -81,129 +175,21 @@ public class Ximalaya extends CordovaPlugin {
             }
 
             @Override
-            public void onError(int code, String message) {
+            public void onError(int i, String s) {
                 JSONObject json = new JSONObject();
                 try {
-                    json.put("code", code);
-                    json.put("message", message);
+                    json.put("code", i);
+                    json.put("message", s);
                 }
                 catch (Exception e) {
                     e.printStackTrace();
                 }
                 callbackContext.error(json.toString());
             }
-        });
+        };
     }
 
-    private void getAlbumList(JSONArray args, CallbackContext callbackContext) throws JSONException {
-        Map<String, String> map = new HashMap<String, String>();
-        map.put(DTransferConstants.CATEGORY_ID, args.getString(0));
-        map.put(DTransferConstants.CATEGORY_ID ,"0");
-        map.put(DTransferConstants.CALC_DIMENSION ,"1");
-        CommonRequest.getAlbumList(map, new IDataCallBack<AlbumList>() {
-            @Override
-            public void onSuccess(AlbumList object) {
-                JSONObject json = new JSONObject();
-                try {
-                    json.put("code", 0);
-                    json.put("message", "success");
-                    json.put("data", new JSONObject(new Gson().toJson(object)));
-                }
-                catch (Exception e) {
-                    e.printStackTrace();
-                }
-                callbackContext.success(json.toString());
-            }
 
-            @Override
-            public void onError(int code, String message) {
-                JSONObject json = new JSONObject();
-                try {
-                    json.put("code", code);
-                    json.put("message", message);
-                }
-                catch (Exception e) {
-                    e.printStackTrace();
-                }
-                callbackContext.error(json.toString());
-            }
-        });
-    }
-
-    private void getTags(JSONArray args, CallbackContext callbackContext) throws JSONException {
-        Map<String, String> map = new HashMap<String, String>();
-        map.put(DTransferConstants.CATEGORY_ID, args.getString(0));
-        map.put(DTransferConstants.TYPE, "0");
-        CommonRequest.getTags(map, new IDataCallBack<TagList>() {
-            @Override
-            public void onSuccess(TagList object) {
-                JSONObject json = new JSONObject();
-                try {
-                    json.put("code", 0);
-                    json.put("message", "success");
-                    json.put("data", new JSONObject(new Gson().toJson(object)));
-                }
-                catch (Exception e) {
-                    e.printStackTrace();
-                }
-                callbackContext.success(json.toString());
-            }
-
-            @Override
-            public void onError(int code, String message) {
-                JSONObject json = new JSONObject();
-                try {
-                    json.put("code", code);
-                    json.put("message", message);
-                }
-                catch (Exception e) {
-                    e.printStackTrace();
-                }
-                callbackContext.error(json.toString());
-            }
-        });
-    }
-
-    private void getCategories(JSONArray args, CallbackContext callbackContext) throws JSONException {
-        Map<String, String> map = new HashMap<String, String>();
-        CommonRequest.getCategories(map, new IDataCallBack<CategoryList>() {
-            @Override
-            public void onSuccess(CategoryList object) {
-                JSONObject json = new JSONObject();
-                try {
-                    json.put("code", 0);
-                    json.put("message", "success");
-                    json.put("data", new JSONObject(new Gson().toJson(object)));
-                }
-                catch (Exception e) {
-                    e.printStackTrace();
-                }
-                callbackContext.success(json.toString());
-            }
-
-            @Override
-            public void onError(int code, String message) {
-                JSONObject json = new JSONObject();
-                try {
-                    json.put("code", code);
-                    json.put("message", message);
-                }
-                catch (Exception e) {
-                    e.printStackTrace();
-                }
-                callbackContext.error(json.toString());
-            }
-        });
-    }
-
-    private void init(JSONArray args) throws JSONException {
-        String appKey = args.getString(0);
-        String packId = args.getString(1);
-        String appSecret = args.getString(2);
-        CommonRequest.getInstanse().setAppkey(appKey);
-        CommonRequest.getInstanse().setPackid(packId);
-        CommonRequest.getInstanse().init(cordova.getActivity().getApplicationContext(), appSecret);
-    }
 
     /*private void coolMethod(String message, CallbackContext callbackContext) {
         if (message != null && message.length() > 0) {
--- www/ximalaya.js
+++ www/ximalaya.js
@@ -4,18 +4,29 @@ exports.init = function (arg0, arg1, arg2, success, error) {
     exec(success, error, 'ximalaya', 'init', [arg0, arg1, arg2]);
 }
 
-exports.getCategories = function (arg0, success, error) {
-  exec(success, error, 'ximalaya', 'getCategories', [arg0]);
+/**
+ *
+ * @param arg0
+ * @param arg1
+ * @param suucess
+ * @param error
+ */
+exports.callApi = function (arg0, arg1, suucess, error) {
+  exec(success, error, 'ximalaya', arg0, [arg1]);
 }
 
-exports.getTags = function (arg0, success, error) {
-  exec(success, error, 'ximalaya', 'getTags', [arg0]);
-}
-
-exports.getAlbumList = function (arg0, arg1, success, error) {
-  exec(success, error, 'ximalaya', 'getAlbumList', [arg0, arg1]);
-}
-
-exports.getCategoryRecommendAlbums = function (arg0, arg1, success, error) {
-  exec(success, error, 'ximalaya', 'getCategoryRecommendAlbums', [arg0, arg1]);
-}
+// exports.getCategories = function (arg0, success, error) {
+//   exec(success, error, 'ximalaya', 'getCategories', [arg0]);
+// }
+//
+// exports.getTags = function (arg0, success, error) {
+//   exec(success, error, 'ximalaya', 'getTags', [arg0]);
+// }
+//
+// exports.getAlbumList = function (arg0, arg1, success, error) {
+//   exec(success, error, 'ximalaya', 'getAlbumList', [arg0, arg1]);
+// }
+//
+// exports.getCategoryRecommendAlbums = function (arg0, arg1, success, error) {
+//   exec(success, error, 'ximalaya', 'getCategoryRecommendAlbums', [arg0, arg1]);
+// }
